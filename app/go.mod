ea5e87b02a67bb4dc6412862be6b0f443f155c1c
Qiu <alex@alexslomka.xyz>
2024-03-01 20:32:18 +0100

    all: create basic dir structure

---
+++ app/go.mod
@@ -0,0 +1,5 @@
+module cellmaster/app
+
+go 1.22.0
+
+require github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240118000515-a250818d05e3
---
+++ app/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"runtime"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
