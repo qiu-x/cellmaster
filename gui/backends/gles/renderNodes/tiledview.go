053cb32be41a58852129138e959c394a694b0173
Qiu <alex@alexslomka.xyz>
2024-03-25 21:39:38 +0100

    gui/backends/gles: more rendering...

--- gui/backends/gles/renderNodes/tiledview.go
+++ gui/backends/gles/renderNodes/tiledview.go
@@ -4,48 +4,40 @@ import (
 	"cellmaster/gui"
 	"cellmaster/gui/backends/renderGraph"
 	"cellmaster/gui/layouts"
+	"fmt"
+	"strings"
 
-	"github.com/go-gl/gl/v2.1/gl"
+	gl "github.com/go-gl/gl/v3.1/gles2"
 )
 
 var VertexShader = `
-#version 330
-uniform vec2 window_size;
+#version 300 es
+precision highp float;
 in vec3 vert;
-in vec2 vertTexCoord;
-out vec2 fragTexCoord;
 
 void main() {
-	mat4 window_scale = mat4 (
-		vec4(window_size.x/window_size.y, 0.0, 0.0, 0.0),
-		vec4(0.0, 1.0, 0.0, 0.0),
-		vec4(0.0, 0.0, 1.0, 0.0),
-		vec4(0.0, 0.0, 0.0, 1.0)
-	);
-	mat4 transform = mat4 (
-		vec4(1.0, 0.0, 0.0, 0.0),
-		vec4(0.0, 1.0, 0.0,-0.95),
-		vec4(0.0, 0.0, 1.0, 0.0),
-		vec4(0.0, 0.0, 0.0, 1.0)
-	);
-    fragTexCoord = vertTexCoord;
-    gl_Position = vec4(vert, 1) * transform * window_scale;
+    gl_Position = vec4(vert, 1);
 }
 ` + "\x00"
 
 var FragmentShader = `
-#version 330
+#version 300 es
+precision highp float;
+
 uniform sampler2D tex;
-in vec2 fragTexCoord;
+in highp vec2 fragTexCoord;
 out vec4 outputColor;
+
 void main() {
-	outputColor = texture(tex, fragTexCoord);
+    outputColor = vec4(0,0,1, 1);
 }
 ` + "\x00"
 
 type TiledView struct {
 	renderGraph.RenderNodeBase
-	reference layouts.TiledView
+	reference *layouts.TiledView
+	shader uint32
+	vao uint32
 }
 
 var bgVerts = []float32{
@@ -55,24 +47,87 @@ var bgVerts = []float32{
 	1.0, -1.0, 0.1, 1.0, 1.0,
 }
 
-func (t *TiledView) Load(tiledview gui.IContainer) {
-	t.reference = tiledview.(layouts.TiledView)
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
 
-	program, err := newProgram(VertexShader, FragmentShader)
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+}
+
+func (t *TiledView) Load(tiledview gui.IContainer) {
+	t.reference = tiledview.(*layouts.TiledView)
+
+	var err error
+	t.shader, err = newProgram(VertexShader, FragmentShader)
 	if err != nil {
 		panic(err)
 	}
 
-	var vao uint32
-	gl.GenVertexArrays(1, &text_vao)
-	gl.BindVertexArray(text_vao)
+	gl.GenVertexArrays(1, &t.vao)
+	gl.BindVertexArray(t.vao)
 
 	var vbo uint32
-	gl.GenBuffers(1, &text_vbo)
-	gl.BindBuffer(gl.ARRAY_BUFFER, text_vbo)
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
 	gl.BufferData(gl.ARRAY_BUFFER, len(bgVerts)*4, gl.Ptr(bgVerts), gl.STATIC_DRAW)
-}
+
+	vertAttrib := uint32(gl.GetAttribLocation(t.shader, gl.Str("vert\x00")))
+	gl.EnableVertexAttribArray(vertAttrib)
+	gl.VertexAttribPointerWithOffset(vertAttrib, 3, gl.FLOAT, false, 5*4, 0)}
 
 func (t *TiledView) Render() {
-	panic("unimplemented")
+	gl.UseProgram(t.shader)
+	gl.BindVertexArray(t.vao)
+	gl.DrawArrays(gl.TRIANGLE_STRIP, 0, 4)
 }
