ddda8bdf1a49e3218055642083763c339d29bebe
Qiu <alex@alexslomka.xyz>
2024-05-03 23:20:59 +0200

    app: basic tiling

--- app/main.go
+++ app/main.go
@@ -1,20 +1,104 @@
 package main
 
 import (
-	"cellmaster/gui/backends"
-	"cellmaster/gui/scenegraph"
-	"runtime"
+	"image"
+	"image/color"
+	"log"
+	"os"
+
+	"gioui.org/app"
+	"gioui.org/font"
+	"gioui.org/font/gofont"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
+	"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget/material"
+	"gioui.org/x/component"
 )
 
-func init() {
-	runtime.LockOSThread()
+func main() {
+	th := NewTheme(gofont.Collection())
+	ui := UI{
+		Window: new(app.Window),
+		Theme:  th,
+		Resize: component.Resize{Ratio: 0.5},
+	}
+	go func() {
+		if err := ui.Loop(); err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
 }
 
-func main() {
-	renderer := backends.GetBestBackend()
-	window := renderer.GetWindow()
-	defer renderer.CleanUp()
+type (
+	C = layout.Context
+	D = layout.Dimensions
+)
 
-	scene := scenegraph.NewScene(nil, window)
-	renderer.RenderLoop(scene)
+type UI struct {
+	Window *app.Window
+	Theme  *Theme
+	component.Resize
+}
+
+type Theme struct {
+	Base *material.Theme
+}
+
+func NewTheme(font []font.FontFace) *Theme {
+	th := material.NewTheme()
+	th.Shaper = text.NewShaper(text.WithCollection(font))
+	return &Theme{
+		Base: th,
+	}
+}
+
+func (ui UI) Loop() error {
+	var ops op.Ops
+	for {
+		e := ui.Window.Event()
+		switch e := e.(type) {
+		case app.DestroyEvent:
+			return e.Err
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+			ui.Layout(gtx)
+			e.Frame(gtx.Ops)
+		}
+	}
+}
+
+func (ui *UI) Layout(gtx C) D {
+	return ui.Resize.Layout(gtx,
+		func(gtx C) D {
+			return layout.UniformInset(unit.Dp(4)).Layout(gtx, func(gtx C) D {
+				c := color.NRGBA{R: 40, G: 40, B: 40, A: 255}
+				paint.FillShape(gtx.Ops, c, clip.Rect{Max: gtx.Constraints.Max}.Op())
+				return D{Size: gtx.Constraints.Max}
+
+			})
+		},
+		func(gtx C) D {
+			return layout.UniformInset(unit.Dp(4)).Layout(gtx, func(gtx C) D {
+				c := color.NRGBA{R: 40, G: 40, B: 40, A: 255}
+				paint.FillShape(gtx.Ops, c, clip.Rect{Max: gtx.Constraints.Max}.Op())
+				return D{Size: gtx.Constraints.Max}
+			})
+		},
+		func(gtx C) D {
+			rect := image.Rectangle{
+				Max: image.Point{
+					X: (gtx.Dp(unit.Dp(4))),
+					Y: (gtx.Constraints.Max.Y),
+				},
+			}
+			paint.FillShape(gtx.Ops, color.NRGBA{A: 255}, clip.Rect(rect).Op())
+			return D{Size: rect.Max}
+		},
+	)
 }
